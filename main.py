e4b79d13d81e99e4723691e728662074536b8916
artem <vrail30@outlook.com>
2021-10-18 19:44:27 +0300

    Task done.

--- main.py
+++ main.py
@@ -2,4 +2,31 @@ from typing import List
 
 
 def count_words(sentences: List[str], word: str) -> int:
-    raise NotImplementedError('Implement me!')
+    """
+        Take a list of strings and return the list of their hashes
+
+        Parameters
+        ----------
+        sentences: List[str]
+            List of some random strings, which contain or don't contain
+            parameter word
+        word: str
+            Word which entries to sentences we need to count.
+
+        Returns
+        ---------
+        int
+            Number of "word" entries to "sentences" .
+
+        Examples
+        --------
+        names = ['Alexey', 'Ivan', 'Petr']
+        hash_names(names)
+        [-6913778901462750956, 4044914442677255742, -8154646851311137882]
+        """
+    from functools import reduce
+
+    return reduce(
+        lambda num1, num2: num1 + num2,
+        map(lambda string: string.count(word), sentences)
+    )
